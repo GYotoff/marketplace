79bdb2f4b5f776ac6c448866813b1207487c817e
GYotoff <georgi.s.yotov@gmail.com>
2026-04-08 17:25:28 +0300

    Add files via upload

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -260,9 +260,9 @@ function SuperAdminDashboard({ profile }) {
       icon: 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4',
     },
     {
-      label: 'All users',
+      label: 'Entity management',
       desc: `${counts.users} registered`,
-      to: '/admin/users',
+      to: '/admin/entities',
       icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z',
     },
     {
